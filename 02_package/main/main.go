c62f2acae19876d492a97b90e885875e5c200578
Galen Johnson <solitaryr@gmail.com>
2016-11-22 21:38:23 -0500

    Added package section

---
+++ 02_package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"udemyGolangTraining/02_package/icomefromalaska"
+	"udemyGolangTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
