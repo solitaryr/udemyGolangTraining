800dbae2387b40558f783982d8a89e40209d69a0
Galen Johnson <solitaryr@gmail.com>
2016-11-23 20:04:16 -0500

    All together

--- 03_variables/03_less-emphasis/main.go
+++ 03_variables/03_less-emphasis/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+var o = "this is stored in the variable o"     // package scope
+var p, q string = "stored in p", "stored in q" // package scope
+var r string                                   // package scope
+
 func main() {
 	// declare single
 	var message string
@@ -24,6 +28,16 @@ func main() {
 	m := 4
 	n := true
 
+	// All together now
+	o = "stored in d" // declaration above; assignment here; package scope
+	var s = 42        // function scope - subsequent variables have func scope:
+	t := 43
+	u := "stored in g"
+	v, w := "stored in h", "stored in i"
+	x, y, z, aa := 44.7, true, false, 'm' // single quotes
+	ab := "n"                             // double quotes
+	ac := `o`                             // back ticks
+
 	fmt.Println(message)
 	fmt.Println(a, b, c)
 	fmt.Println(d, e, f)
@@ -32,4 +46,21 @@ func main() {
 	fmt.Printf("%v %T\n", i, i)
 	fmt.Println(message1)
 	fmt.Println(j, k, l, m, n)
+
+	fmt.Println("All together now!")
+	fmt.Println("o - ", o)
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+	fmt.Println("r - ", r)
+	fmt.Println("s - ", s)
+	fmt.Println("t - ", t)
+	fmt.Println("u - ", u)
+	fmt.Println("v - ", v)
+	fmt.Println("w - ", w)
+	fmt.Println("x - ", x)
+	fmt.Println("y - ", y)
+	fmt.Println("z - ", z)
+	fmt.Println("aa - ", aa)
+	fmt.Println("ab - ", ab)
+	fmt.Println("ac - ", ac)
 }
