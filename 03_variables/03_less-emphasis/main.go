1c9c3700642842e72fa5cc1dd5c4d79c95562ab5
Galen Johnson <solitaryr@gmail.com>
2016-11-23 19:47:43 -0500

    declare many at once, fix comments

--- 03_variables/03_less-emphasis/main.go
+++ 03_variables/03_less-emphasis/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-  # declare single
-  var message string
-  message = "Hello World!"
-  fmt.Println(message)
+	// declare single
+	var message string
+	message = "Hello World!"
+
+	// declare multiple
+	var a, b, c int
+	a = 1
+
+	fmt.Println(message)
+	fmt.Println(a, b, c)
 }
