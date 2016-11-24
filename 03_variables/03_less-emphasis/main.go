50605c5e9d48c1b7bbf9c1ba83bc3713fa41a522
Galen Johnson <solitaryr@gmail.com>
2016-11-23 19:56:25 -0500

    Init shorthand

--- 03_variables/03_less-emphasis/main.go
+++ 03_variables/03_less-emphasis/main.go
@@ -17,11 +17,19 @@ func main() {
 	// infer type
 	var g, h, i = 3, false, '4'
 
+	// init shorthand
+	// you can only do this inside a func
+	message1 := "Hello World!"
+	j, k, l := 1, false, 3
+	m := 4
+	n := true
+
 	fmt.Println(message)
 	fmt.Println(a, b, c)
 	fmt.Println(d, e, f)
 	fmt.Printf("%v %T\n", g, g)
 	fmt.Printf("%v %T\n", h, h)
 	fmt.Printf("%v %T\n", i, i)
-
+	fmt.Println(message1)
+	fmt.Println(j, k, l, m, n)
 }
