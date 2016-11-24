b49ed53a77a923c25ed370c953c31f8fec63c42e
Galen Johnson <solitaryr@gmail.com>
2016-11-23 19:53:29 -0500

    infer type from value

--- 03_variables/03_less-emphasis/main.go
+++ 03_variables/03_less-emphasis/main.go
@@ -14,7 +14,14 @@ func main() {
 	// initialize many at once
 	var d, e, f int = 1, 2, 3
 
+	// infer type
+	var g, h, i = 3, false, '4'
+
 	fmt.Println(message)
 	fmt.Println(a, b, c)
 	fmt.Println(d, e, f)
+	fmt.Printf("%v %T\n", g, g)
+	fmt.Printf("%v %T\n", h, h)
+	fmt.Printf("%v %T\n", i, i)
+
 }
